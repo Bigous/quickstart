0015d1de4862f2e001f436107dd5cab315dafa79
Richard Natal <bigous@gmail.com>
2016-08-19 14:19:44 -0300

    Using ng2-highcharts

--- systemjs.config.js
+++ systemjs.config.js
@@ -10,7 +10,9 @@
 
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs'
+    'rxjs': 'node_modules/rxjs',
+    'ng2-highcharts': 'node_modules/ng2-highcharts',
+    'highcharts': 'node_modules/highcharts'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -18,6 +20,8 @@
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    'ng2-highcharts': { main: 'index.js', defaultExtension: 'js' },
+    'highcharts': { main: 'highcharts.js', defaultExtension: 'js'}
   };
 
   var ngPackageNames = [
